77e07ddcbbde853e59031bcf3edeb8cbca99ff4a
jwets <julien.wets@gmail.com>
2019-08-25 03:02:25 +0200

    Changed links in navbar

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -11,7 +11,7 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 class SecurityController extends AbstractController
 {
     /**
-     * @Route("/login", name="app_login")
+     * @Route("/connexion", name="app_login")
      */
     public function login(AuthenticationUtils $authenticationUtils, Request $request): Response
     {
@@ -31,7 +31,7 @@ class SecurityController extends AbstractController
     }
 
     /**
-     * @Route("/logout", name="app_logout")
+     * @Route("/déconnexion", name="app_logout")
      */
     public function logout()
     {
