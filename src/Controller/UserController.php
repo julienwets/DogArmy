03d5499f77ba32577f9b6ac0f3f8e2129a8e8c9f
jwets <julien.wets@gmail.com>
2019-09-23 02:59:24 +0200

    Fixed a bug that made the message form not appear

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -19,38 +19,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
  */
 class UserController extends AbstractController
 {
-    // /**
-    //  * @Route("/", name="user_index", methods={"GET"})
-    //  */
-    // public function index(UserRepository $userRepository): Response
-    // {
-    //     return $this->render('user/index.html.twig', [
-    //         'users' => $userRepository->findAll(),
-    //     ]);
-    // }
-
-    // /**
-    //  * @Route("/new", name="user_new", methods={"GET","POST"})
-    //  */
-    // public function new(Request $request): Response
-    // {
-    //     $user = new User();
-    //     $form = $this->createForm(UserType::class, $user);
-    //     $form->handleRequest($request);
-
-    //     if ($form->isSubmitted() && $form->isValid()) {
-    //         $entityManager = $this->getDoctrine()->getManager();
-    //         $entityManager->persist($user);
-    //         $entityManager->flush();
-
-    //         return $this->redirectToRoute('user_index');
-    //     }
-
-    //     return $this->render('user/new.html.twig', [
-    //         'user' => $user,
-    //         'form' => $form->createView(),
-    //     ]);
-    // }
 
     /**
      * @Route("/{id}", name="user_show", methods={"GET", "POST"})
@@ -134,27 +102,6 @@ class UserController extends AbstractController
             'confirmFormObject' => $confirmForm,
         ]);
     }
-
-    // /**
-    //  * @Route("/{id}/edit", name="user_edit", methods={"GET","POST"})
-    //  */
-    // public function edit(Request $request, User $user): Response
-    // {
-    //     $form = $this->createForm(UserType::class, $user);
-    //     $form->handleRequest($request);
-
-    //     if ($form->isSubmitted() && $form->isValid()) {
-    //         $this->getDoctrine()->getManager()->flush();
-    //         $this->imageFile = null;
-
-    //         return $this->redirectToRoute('user_index');
-    //     }
-
-    //     return $this->render('user/edit.html.twig', [
-    //         'user' => $user,
-    //         'form' => $form->createView(),
-    //     ]);
-    // }
 
     /**
      * @Route("/{id}", name="user_delete", methods={"DELETE"})
